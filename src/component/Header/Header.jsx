aeab48ccfc48a988185686a4b83cb0c2af2f5adb
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2023-06-29 18:12:11 +0300

    fix logo

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./header.module.scss";
 import Logo from "../Logo/Logo";
 
+// const tg = window.Telegram.WebApp
+
 export default function Header() {
   return (
     <div className={styles.header}>
