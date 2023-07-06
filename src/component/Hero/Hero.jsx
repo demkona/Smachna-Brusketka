f799ece5a7f143e8cfb1596558e784db010c79a2
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2023-07-06 17:03:33 +0300

    add styles title

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import React from "react";
 import hero from './hero.png'
+import styles from "./hero.module.scss"
 
 export default function Hero() {
   return (
     <div>
       <Image src={hero} width={360} height={360} alt="Hero"></Image>
-      <p>Антипаста</p>
+      <p className={styles.title}>Антипаста</p>
       <Image src={hero} width={360} height={360} alt="Hero"></Image>
-      <p>Антипаста</p>
+      <p className={styles.title}>Антипаста</p>
       <Image src={hero} width={360} height={360} alt="Hero"></Image>
-      <p>Антипаста</p>
+      <p className={styles.title}>Антипаста</p>
       <Image src={hero} width={360} height={360} alt="Hero"></Image>
-      <p>Антипаста</p>
+      <p className={styles.title}>Антипаста</p>
     </div>
   );
 }
