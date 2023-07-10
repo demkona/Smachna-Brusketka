3a2279890c471f7e88d578ef896f31a0ce3f41e9
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2023-07-10 19:18:21 +0300

    add hero

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -2,63 +2,98 @@ import Image from "next/image";
 import React from "react";
 import hero from "./hero.png";
 import styles from "./hero.module.scss";
+import Link from "next/link";
 
 export default function Hero() {
   return (
     <ul className={styles.list}>
       <li className={styles.item}>
-        {" "}
-        <Image src={hero} width={300} height={300}alt="Hero"></Image>
+      <Link href={'/'} target="blank">
+      <Image src={hero} width={200} height={200}alt="Hero"></Image>
         <h1 className={styles.title}>Антипаста</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
           accumsan mauris. Maecenas sed justo.
         </p>
+      </Link>
+        {" "}
       </li>
       <li className={styles.item}>
-        {" "}
-        <Image src={hero} width={300} height={300} alt="Hero"></Image>
+      <Link href={'/'} target="blank">
+      <Image src={hero} width={200} height={200}alt="Hero"></Image>
         <h1 className={styles.title}>Антипаста</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
           accumsan mauris. Maecenas sed justo.
         </p>
+      </Link>
+        {" "}
       </li>
       <li className={styles.item}>
-        {" "}
-        <Image src={hero} width={300} height={300} alt="Hero"></Image>
+      <Link href={'/'} target="blank">
+      <Image src={hero} width={200} height={200}alt="Hero"></Image>
         <h1 className={styles.title}>Антипаста</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
           accumsan mauris. Maecenas sed justo.
         </p>
+      </Link>
+        {" "}
       </li>
       <li className={styles.item}>
-        {" "}
-        <Image src={hero} width={300} height={300} alt="Hero"></Image>
+      <Link href={'/'} target="blank">
+      <Image src={hero} width={200} height={200}alt="Hero"></Image>
         <h1 className={styles.title}>Антипаста</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
           accumsan mauris. Maecenas sed justo.
         </p>
+      </Link>
+        {" "}
       </li>
       <li className={styles.item}>
-        {" "}
-        <Image src={hero} width={300} height={300} alt="Hero"></Image>
+      <Link href={'/'} target="blank">
+      <Image src={hero} width={200} height={200}alt="Hero"></Image>
         <h1 className={styles.title}>Антипаста</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
           accumsan mauris. Maecenas sed justo.
         </p>
+      </Link>
+        {" "}
       </li>
       <li className={styles.item}>
-        {" "}
-        <Image src={hero} width={300} height={300} alt="Hero"></Image>
+      <Link href={'/'} target="blank">
+      <Image src={hero} width={200} height={200}alt="Hero"></Image>
         <h1 className={styles.title}>Антипаста</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
           accumsan mauris. Maecenas sed justo.
         </p>
+      </Link>
+        {" "}
+      </li>
+      <li className={styles.item}>
+      <Link href={'/'} target="blank">
+      <Image src={hero} width={200} height={200}alt="Hero"></Image>
+        <h1 className={styles.title}>Антипаста</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
+          accumsan mauris. Maecenas sed justo.
+        </p>
+      </Link>
+        {" "}
+      </li>
+      <li className={styles.item}>
+      <Link href={'/'} target="blank">
+      <Image src={hero} width={200} height={200}alt="Hero"></Image>
+        <h1 className={styles.title}>Антипаста</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
+          accumsan mauris. Maecenas sed justo.
+        </p>
+      </Link>
+        {" "}
       </li>
     </ul>
   );
--- src/component/Navigation/Nav.jsx
+++ src/component/Navigation/Nav.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-// import Button from "../Button/Button";
 import Link from "next/link";
 import styles from './nav.module.scss'
 
 export default function Nav() {
   return (
-    <div>
-      <div>
+      <div className={styles.container}>
         <Link className={styles.link} href={"/"}>Головна сторінка</Link>
         <Link className={styles.link} href={"blog"}>Блог</Link>
         <Link className={styles.link} href={"about"}>Про нас</Link>
       </div>
-      {/* <Button>Увійти</Button>/ */}
-    </div>
   );
 }
