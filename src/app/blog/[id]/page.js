b4219f2c139f8266b27c6b92e560581e86ef8afe
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2023-07-10 14:34:56 +0300

    fix id

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,4 +1,4 @@
-export default async function Post({ id }) {
+export default async function Post({params:{ id }}) {
   
     return (
       <>
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./header.module.scss";
 import Logo from "../Logo/Logo";
-import Link from "next/link";
 import Nav from "../Navigation/Nav";
 
 export default function Header() {
