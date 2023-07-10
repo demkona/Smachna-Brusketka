816089166988ed7803e74b540ec20908f13b0640
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2023-07-10 22:01:48 +0300

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,6 @@ import styles from "./page.module.scss";
 export default function About() {
   return (
     <div className={styles.container}>
-      <h1>Про нас</h1>
       <Image src={about} alt="About">
       </Image>
     </div>
